8349130d1a13adec88a93112b844ae0398fc9b70
yutaro <yutaro256256@gmail.com>
2019-10-14 03:47:14 +0000

    add column,validation

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -54,7 +54,7 @@ class TasksController < ApplicationController
     private
 
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
   end
 
 end
